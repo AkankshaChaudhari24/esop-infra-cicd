0b5dedb05f48b2c6ffb47355ca862422f90ae51a
AkankshaChaudhari24 <akankshac@sahaj.ai>
2023-03-27 16:56:09 +0530

    add ingress block

--- Terraform/ec2/main.tf
+++ Terraform/ec2/main.tf
@@ -61,6 +61,15 @@ resource "aws_security_group" "akanksha_tls" {
     cidr_blocks = ["0.0.0.0/0"]
   }
 
+
+  ingress {
+    description = "TLS from VPC"
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
   egress {
     from_port        = 0
     to_port          = 0
